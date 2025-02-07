ba97ec9f74b48c5d777e1f219f829fff3d78a2ab
IQBit <iqnitetechnologies@gmail.com>
2025-02-07 09:29:41 +0100

    Add launcher

---
+++ game.py
@@ -0,0 +1,3 @@
+def draw():
+    screen.fill((0, 0, 0))
+    screen.draw.text("Hello, world!", (100, 100), color=(255, 255, 255))
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import pgzero
+import pgzrun
+import game
+
+if __name__ == "__main__":
+    pgzrun.go()
