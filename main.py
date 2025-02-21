4a14f16688c545efadcbd86e708a92e8c0ac4187
IQBit <iqnitetechnologies@gmail.com>
2025-02-21 12:10:19 +0100

    FIx list bug

--- main.py
+++ main.py
@@ -8,31 +8,37 @@ pygame.init()
 ctx = GameContext(screen=pygame.display.set_mode((0, 0)))  # (0, 0) means full screen
 
 level = Level.load(ctx, "level.txt", "images/level{}.png")
-players = []
-players.append (Player(
-    ctx=ctx,
-    image_path="images/player0.png",
-    pos_vector=pygame.Vector2(ctx.screen.get_width() / 2, ctx.screen.get_height() / 2),
-    controls={"left": pygame.K_a, "right": pygame.K_d, "jump": pygame.K_w},
-    move_acceleration=300,
-    friction=0.25,
-    jump_acceleration=3000,
-    gravity=300,
-))
 
-players.append (Player(
-    ctx=ctx,
-    image_path="images/player1.png",
-    pos_vector=pygame.Vector2(ctx.screen.get_width() / 2 + 100, ctx.screen.get_height() / 2 + 100),
-    controls={"left": pygame.K_LEFT, "right": pygame.K_RIGHT, "jump": pygame.K_UP},
-    move_acceleration=300,
-    friction=0.25,
-    jump_acceleration=3000,
-    gravity=300,
-))
+players = [
+    Player(
+        ctx=ctx,
+        image_path="images/player0.png",
+        pos_vector=pygame.Vector2(
+            ctx.screen.get_width() / 2, ctx.screen.get_height() / 2
+        ),
+        controls={"left": pygame.K_a, "right": pygame.K_d, "jump": pygame.K_w},
+        move_acceleration=300,
+        friction=0.25,
+        jump_acceleration=3000,
+        gravity=300,
+    ),
+    Player(
+        ctx=ctx,
+        image_path="images/player1.png",
+        pos_vector=pygame.Vector2(
+            ctx.screen.get_width() / 2 + 100, ctx.screen.get_height() / 2 + 100
+        ),
+        controls={"left": pygame.K_LEFT, "right": pygame.K_RIGHT, "jump": pygame.K_UP},
+        move_acceleration=300,
+        friction=0.25,
+        jump_acceleration=3000,
+        gravity=300,
+    ),
+]
 
-ctx.objects["players"] = players
 ctx.objects["level"] = level
+for i, player in enumerate(players):
+    ctx.objects[f"player{i}"] = player
 
 while ctx.running:
     # poll for events
@@ -45,11 +51,9 @@ while ctx.running:
     ctx.screen.fill("black")
 
     level.draw()
-    for i in range (0, len(players)):
-        players[i].read_controls()
-        players[i].simulate()
-    
-
+    for player in players:
+        player.read_controls()
+        player.simulate()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
