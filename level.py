5716fc768d76f9260f0577ad3a43297f941a3526
jamesfabi <mahn.fabi@gmail.com>
2025-02-24 11:30:37 +0100

    map

--- level.py
+++ level.py
@@ -4,22 +4,22 @@ from engine import MultiSprite
 
 class Level(MultiSprite):
     @classmethod
-    def load(cls, game, pos_filepath, image_filepath):
+    def load(cls, ctx, pos_filepath, image_filepath):   
         with open(pos_filepath) as f:
             data = f.read().splitlines()
         return cls(
-            game,
+            ctx,
             [
                 {
                     "image_path": image_filepath.format(i),
                     "pos_vector": pygame.Vector2(
-                        game.screen.get_width() / 2 + int(pos.split(",")[0]),
-                        game.screen.get_height() / 2 + int(pos.split(",")[1]),
+                        ctx.screen.get_width() / 2 + int(pos.split(",")[0]),
+                        ctx.screen.get_height() / 2 + int(pos.split(",")[1]),
                     ),
                 }
                 for i, pos in enumerate(data)
             ],
         )
 
-    def __init__(self, game, sprite_args):
-        super().__init__(game, sprite_args)
+    def __init__(self, ctx, sprite_args):
+        super().__init__(ctx, sprite_args)
