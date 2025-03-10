5827ad4491d9ed609c14890c6c1f5be61cbc06a5
iqnite <iqnitetechnologies@gmail.com>
2025-03-10 11:13:47 +0100

    Improve attacks

--- attacks.py
+++ attacks.py
@@ -2,18 +2,21 @@ from engine import Game, Sprite
 from player import Player
 
 
-class ShootAttack(Sprite):
-    def __init__(self, game: Game, x_velocity=0, y_velocity=0, **kwargs):
+class Attack(Sprite):
+    def __init__(self, game: Game, x_velocity=0, y_velocity=0, damage=5, **kwargs):
         super().__init__(game, **kwargs)
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
+        self.damage = damage
 
+
+class ShootAttack(Attack):
     def loop(self):
         self.x_move(self.x_velocity)
         self.y_move(self.y_velocity)
         if self.colliding():
             for obj in self.game.objects:
                 if self.collides_with(obj) and isinstance(obj, Player):
-                    obj.on_hit()
+                    obj.on_hit(self)
             self.game.remove_object(self)
         super().loop()
